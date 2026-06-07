47d0b1f3479459edad170ef6ac77448edc5a02f0
zydezu <kinomaxxing@gmail.com>
2026-06-07 20:06:15 +0100

    FIX: fix linux building

--- tools/build_executable.sh
+++ tools/build_executable.sh
@@ -13,12 +13,11 @@ fi
 uv pip install --python "$BUILD_VENV/bin/python" pyinstaller -r "$ROOT_DIR/requirements.txt"
 
 echo "[packaging] Cleaning previous builds..."
+rm -rf "$ROOT_DIR/dist" "$ROOT_DIR/build" "$ROOT_DIR/buildhooks" "$ROOT_DIR"/*.spec
 
 echo "[packaging] Building..."
 uv run --python "$BUILD_VENV/bin/python" pyinstaller --onefile --name "RiiTag-RPC_Linux_x64" \
   --add-data "${ROOT_DIR}/banner.txt:." \
   --add-data "${ROOT_DIR}/config.json:." \
   "$ROOT_DIR/start.py"
-
-rm -rf "$ROOT_DIR/dist" "$ROOT_DIR/build" "$ROOT_DIR/buildhooks" "$ROOT_DIR"/*.spec
 echo "[packaging] Done. Executable is in $ROOT_DIR/dist/RiiTag-RPC_Linux_x64"
